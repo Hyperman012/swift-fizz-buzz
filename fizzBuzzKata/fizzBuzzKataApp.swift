a11e5f84bdab5db83c4a316b92c926c3b0d1ced5
Kevin Sivic <kevin.sivic@gmail.com>
2022-01-04 09:54:59 -0500

    Initial Commit

---
+++ fizzBuzzKata/fizzBuzzKataApp.swift
@@ -0,0 +1,17 @@
+//
+//  fizzBuzzKataApp.swift
+//  fizzBuzzKata
+//
+//  Created by Kevin Sivic on 1/4/22.
+//
+
+import SwiftUI
+
+@main
+struct fizzBuzzKataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
