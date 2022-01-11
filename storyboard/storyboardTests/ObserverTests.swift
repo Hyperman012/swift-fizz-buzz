4c728a85a589989c18ca2397eee3720e6cd433af
Adam W <abwn83@gmail.com>
2022-01-11 11:36:52 -0600

    Get latest message in Observer explorartory test.

--- storyboard/storyboardTests/ObserverTests.swift
+++ storyboard/storyboardTests/ObserverTests.swift
@@ -3,8 +3,24 @@ import Foundation
 
 
 class Observer {
+    var subscriber: Any!
+    var latestMessage = ""
+
+    init() {
+        subscriber = NotificationCenter.default.addObserver(self, selector: #selector(updateMessage(_:)), name: NSNotification.Name("m1"), object: nil)
+    }
+
+    @objc private func updateMessage(_ message: Notification)
+    {
+        latestMessage = message.object as! String
+    }
+
     func addMessage(_ message: String) {
         NotificationCenter.default.post(name: NSNotification.Name("m1"), object: message)
+    }
+
+    func readReceivedMessage() -> String {
+        latestMessage
     }
 }
 
@@ -19,10 +35,28 @@ class ObserverTests: XCTestCase {
         let notification = XCTNSNotificationExpectation(name: notificationName, object: expectedMessage)
 
         // act.
-        let observer = Observer().addMessage(expectedMessage)
+        Observer().addMessage(expectedMessage)
 
         // assert.
         let result = XCTWaiter.wait(for: [notification], timeout: 1)
         XCTAssertTrue(result == .completed)
+    }
+
+    public func testShouldReceiveMessage() {
+        // arrange.
+        let expectedMessage = "string"
+
+        let notificationName = NSNotification.Name("m1")
+        let notification = XCTNSNotificationExpectation(name: notificationName, object: expectedMessage)
+
+        let observer = Observer()
+        observer.addMessage(expectedMessage)
+        _ = XCTWaiter.wait(for: [notification], timeout: 1)
+
+        // act.
+        let actualMessage = observer.readReceivedMessage()
+
+        // assert.
+        XCTAssertEqual(expectedMessage, actualMessage)
     }
 }
