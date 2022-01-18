8ed29c3ca62ae8f42d381e9ba39c7aa78c0877db
Kevin Sivic <kevin.sivic@gmail.com>
2022-01-18 10:53:15 -0500

    spiked FizzyBuzzy showing in the collection

--- storyboard/storyboard/View/FizzBuzzCollectionViewController.swift
+++ storyboard/storyboard/View/FizzBuzzCollectionViewController.swift
@@ -1,33 +1,37 @@
 import Foundation
 import UIKit
+import FizzBuzz
 
 public class FizzBuzzCollectionViewController: UICollectionViewController {
 }
 
 public extension FizzBuzzCollectionViewController {
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
-      return 1
+        return 10
     }
     
-    // 2
-    override func collectionView(
-      _ collectionView: UICollectionView,
-      numberOfItemsInSection section: Int
-    ) -> Int {
-      return 1
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 2
     }
     
-    // 3
-    override func collectionView(
-      _ collectionView: UICollectionView,
-      cellForItemAt indexPath: IndexPath
-    ) -> UICollectionViewCell {
-      let cell = collectionView.dequeueReusableCell(
-        withReuseIdentifier: "fizzbuzz",
-        for: indexPath)
-      cell.backgroundColor = .black
-      // Configure the cell
-      return cell
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath ) -> UICollectionViewCell {
+        let row = indexPath.section
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "fizzbuzz", for: indexPath)
+        let title = UILabel(frame: CGRect(x: 0, y: 0, width: cell.bounds.size.width, height: 50))
+        
+        if indexPath.item == 0 {
+            title.text = String(row)
+        }
+        else
+        {
+            title.text = FizzyBuzzy().calculate(row)
+        }
+        
+        title.font = UIFont(name: "AvenirNext-Bold", size: 15)
+        title.textAlignment = .center
+        cell.contentView.addSubview(title)
+        
+        return cell
     }
 }
  
