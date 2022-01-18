6592492670c21507c757386a356f6222d335d8fb
Kevin Sivic <kevin.sivic@gmail.com>
2022-01-18 14:41:09 -0500

    generate input data programmatically

--- storyboard/storyboard/View/FizzBuzzCollectionViewController.swift
+++ storyboard/storyboard/View/FizzBuzzCollectionViewController.swift
@@ -3,12 +3,13 @@ import UIKit
 import FizzBuzz
 
 public class FizzBuzzCollectionViewController: UICollectionViewController {
-    var dataSource: [Int:String?]
+    var dataSource: [Int:String?] = [:]
 
     public required init?(coder: NSCoder) {
-        dataSource = [1:nil, 2:nil, 3:nil, 4:nil, 5:nil, 6:nil, 7:nil, 8:nil, 9:nil, 10:nil]
-        FizzyBuzzy().calculate(&dataSource)
         super.init(coder: coder)
+
+        (1...100).forEach { dataSource[$0] = "" }
+        FizzyBuzzy().calculate(&dataSource)
     }
 }
 
